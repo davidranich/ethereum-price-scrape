287425d896083df7eecf746903a4405d3cb8e265
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-09-17 19:10:35 -0400

    Added README, and removed property from puppeteer object

---
+++ README.md
@@ -0,0 +1,3 @@
+# 💻 iam-david: Ethereum Price Scrape
+
+here is the very simple code that I use to scrape the USD amount of ethereum
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://coinmarketcap.com/currencies/ethereum/');
 
